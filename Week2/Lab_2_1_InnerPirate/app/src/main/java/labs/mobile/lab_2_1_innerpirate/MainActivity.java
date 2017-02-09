cb4a799806116af8a0b5d2322fc651fa4bdd7adb
Alexander Whitehead <alexandercwhitehead@gmail.com>
2017-02-09 16:25:55 +0000

    Completed lab 2.1 task 1 launched the findNameActivity using an explicit intent Completed lab 2.1 task 2 enabled the findShipButton when the pirate name is derived Completed lab 2.1 task 3 launched the chooseShipActivity using an explicit intent Completed lab 2.1 task 4 put extra data into the intent used to start the chooseShipActivity and extracted that data to personalise the greeting Completed lab 2.1 task 5 finished the chooseShipActivity when the chooseShipButton is clicked Completed lab 2.1 task 6 passed results back from the chooseShipActivity, through the findNameActivity all the way back to the mainActivity Completed lab 2.1 task 7 used an Implicit Intent to send the result of our application via SMS Completed lab 2.1 task 8 tidied up the hardcoded strings

--- Week2/Lab_2_1_InnerPirate/app/src/main/java/labs/mobile/lab_2_1_innerpirate/MainActivity.java
+++ Week2/Lab_2_1_InnerPirate/app/src/main/java/labs/mobile/lab_2_1_innerpirate/MainActivity.java
@@ -1,21 +1,54 @@
 package labs.mobile.lab_2_1_innerpirate;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private String m_Result;
+    static final int CHOOSE_NAME_AND_SHIP_REQUEST = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-	
-	public void findNameButtonOnClick(View pView){
 
+    @Override
+    protected  void onActivityResult(int pRequestCode, int pResultCode, Intent pData)
+    {
+        if(pResultCode == RESULT_OK && pRequestCode == CHOOSE_NAME_AND_SHIP_REQUEST)
+        {
+            if(pData.hasExtra("name") && pData.hasExtra("ship"))
+            {
+                TextView pirateName = (TextView)findViewById(R.id.pirateNameTextView);
+                String result = "Arr, 'tis " + pData.getStringExtra("name") + " of the " +
+                                pData.getStringExtra("ship");
+                pirateName.setText(result);
+                m_Result = result;
+
+                Button shareButton = (Button)findViewById(R.id.shareButton);
+                shareButton.setEnabled(true);
+            }
+        }
+    }
+	
+	public void findNameButtonOnClick(View pView)
+    {
+        Intent intent = new Intent(this, findNameActivity.class);
+        startActivityForResult(intent, CHOOSE_NAME_AND_SHIP_REQUEST);
     }
 
-    public void shareButtonOnClick(View pView) {
-
+    public void shareButtonOnClick(View pView)
+    {
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:555555"));
+        intent.putExtra("sms_body", m_Result);
+        startActivity(intent);
     }	
 }
--- Week2/Lab_2_1_InnerPirate/app/src/main/java/labs/mobile/lab_2_1_innerpirate/findNameActivity.java
+++ Week2/Lab_2_1_InnerPirate/app/src/main/java/labs/mobile/lab_2_1_innerpirate/findNameActivity.java
@@ -5,19 +5,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class findNameActivity extends AppCompatActivity {
+public class findNameActivity extends AppCompatActivity
+{
+    private String m_PirateName;
+    static final int CHOOSE_SHIP_REQUEST = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_name);
     }
 
-    public void getNameButtonOnClick(View pView) {
+    @Override
+    protected  void onActivityResult(int pRequestCode, int pResultCode, Intent pData)
+    {
+        if(pResultCode == RESULT_OK && pRequestCode == CHOOSE_SHIP_REQUEST)
+        {
+            if(pData.hasExtra("ship"))
+            {
+                Intent data = new Intent();
+                data.putExtra("ship", pData.getStringExtra("ship"));
+                data.putExtra("name", m_PirateName);
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        }
+    }
+
+    public void getNameButtonOnClick(View pView)
+    {
         Spinner colourSpinner = (Spinner)findViewById(R.id.colourSpinner);
         RadioGroup motiveGroup = (RadioGroup)findViewById(R.id.motiveRadioGroup);
         int selectedRadioIndex = motiveGroup.indexOfChild(findViewById(motiveGroup.getCheckedRadioButtonId()));
@@ -27,13 +49,22 @@ public class findNameActivity extends AppCompatActivity {
         String[] pirateNames = getResources().getStringArray(R.array.pirateNames);
 
         pirateName.setText("Yer name be " + pirateNames[nameIndex]);
+
+        m_PirateName = pirateNames[nameIndex];
+
+        Button findShipButton = (Button)findViewById(R.id.findShipButton);
+        findShipButton.setEnabled(true);
     }
 
-    public void findShipButtonOnClick(View pView) {
-
+    public void findShipButtonOnClick(View pView)
+    {
+        Intent intent = new Intent(this, chooseShipActivity.class);
+        intent.putExtra("pirateName", m_PirateName);
+        startActivityForResult(intent, CHOOSE_SHIP_REQUEST);
     }
 
-    public void setSailAloneButtonOnClick(View pView) {
+    public void setSailAloneButtonOnClick(View pView)
+    {
 
     }
 }
