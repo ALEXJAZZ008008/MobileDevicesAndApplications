7b3d63194afee52c0eea594e406e06f5a2f64da0
483446 <483446@fed9866f-0bf4-4c46-995d-fb1dc66697d1>
2017-03-02 21:16:14 +0000

    Completed lab 5.1 task 1 created a custom view by extending imageView Completed lab 5.1 task 2 animated the bitmaps in a custom view using a separate thread Completed lab 5.1 task 3 launched SeaActivity after the animation is finished Completed lab 5.1 task 4 Created a SeaView class, registered its callbacks and turned the canvas blue! Completed lab 5.1 task 5 Animated rocks on a SurfaceView! Completed lab 5.1 task 6 Can drag a ship around the sea! Completed lab 5.1 task 7 Centred the ship to the touch pointer! Completed lab 5.2 task 1 Created a gesture detector to detect a simple fling gesture

--- 483446/Week5/Lab_5_1_PirateRocks/app/src/main/java/mobile/labs/lab_5_1_piraterocks/SeaActivity.java
+++ 483446/Week5/Lab_5_1_PirateRocks/app/src/main/java/mobile/labs/lab_5_1_piraterocks/SeaActivity.java
@@ -19,82 +19,156 @@ import android.widget.RelativeLayout;
 
 import java.util.Random;
 
-public class SeaActivity extends AppCompatActivity {
+public class SeaActivity extends AppCompatActivity
+{
+    private SeaView m_SeaView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sea);
+
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.seaLayout);
+        m_SeaView = new SeaView(this);
+        relativeLayout.addView(m_SeaView);
+
+        relativeLayout.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                return m_SeaView.onTouch(v, event);
+            }
+        });
     }
 
-    private class SeaView extends SurfaceView implements SurfaceHolder.Callback {
-
-        private Bitmap m_RockBitmap;
+    private class SeaView extends SurfaceView implements SurfaceHolder.Callback
+    {
+        private Bitmap m_RockBitmap, m_ShipBitmap;
         private Random m_RNG;
+        private Paint m_Paint;
+        private SurfaceHolder m_SurfaceHolder;
+        private Thread m_Thread;
+        private float m_ShipX, m_ShipY;
 
         private float m_Rock1X, m_Rock1Y, m_Rock2X, m_Rock2Y, m_Rock3X, m_Rock3Y;
 
-        public SeaView(Context pContext){
+        public SeaView(Context pContext)
+        {
             super(pContext);
             // Get hold of the bitmaps from compiled resources
             m_RockBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.rock);
+            m_ShipBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ship);
 
             // instantiate other objects we will need
             m_RNG = new Random();
 
+            m_Paint = new Paint();
+            m_SurfaceHolder = getHolder();
+            m_SurfaceHolder.addCallback(this);
         }
 
         // returns a random Y value for a rock within the range of the screen height
-        private float getRandomRockY() {
+        private float getRandomRockY()
+        {
             DisplayMetrics displayMetrics = new DisplayMetrics();
             SeaActivity.this.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
             return (float) m_RNG.nextInt(displayMetrics.heightPixels);
         }
 
         // returns a random X value for a rock to the left of the screen
-        private float getRandomRockX() {
+        private float getRandomRockX()
+        {
             DisplayMetrics displayMetrics = new DisplayMetrics();
             SeaActivity.this.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
             return (float) -m_RNG.nextInt(displayMetrics.widthPixels);
         }
 
-        private void MoveRocks() {
+        private void MoveRocks()
+        {
             DisplayMetrics displayMetrics = new DisplayMetrics();
             SeaActivity.this.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
             float screenWidth = displayMetrics.widthPixels;
+
             m_Rock1X += 10;
-            if(m_Rock1X > screenWidth){
+            if(m_Rock1X > screenWidth)
+            {
                 m_Rock1X = getRandomRockX();
                 m_Rock1Y = getRandomRockY();
             }
 
             m_Rock2X += 10;
-            if(m_Rock2X > screenWidth){
+            if(m_Rock2X > screenWidth)
+            {
                 m_Rock2X = getRandomRockX();
                 m_Rock2Y = getRandomRockY();
             }
 
             m_Rock3X += 10;
-            if(m_Rock3X > screenWidth){
+            if(m_Rock3X > screenWidth)
+            {
                 m_Rock3X = getRandomRockX();
                 m_Rock3Y = getRandomRockY();
             }
         }
 
-        @Override
-        public void surfaceChanged(SurfaceHolder pHolder, int pFormat, int pWidth, int pHeight)
-        {}
+        public boolean onTouch(View pView, MotionEvent pEvent)
+        {
+            if(pEvent.getAction() == MotionEvent.ACTION_DOWN || pEvent.getAction() == MotionEvent.ACTION_MOVE)
+            {
+                m_ShipX = pEvent.getX() - (m_ShipBitmap.getHeight() / 2);
+                m_ShipY = pEvent.getY() - (m_ShipBitmap.getWidth() / 2);
+
+                return true;
+            }
+
+            return false;
+        }
 
         @Override
-        public void surfaceCreated(SurfaceHolder pHolder){
+        public void surfaceChanged(SurfaceHolder pHolder, int pFormat, int pWidth, int pHeight)
+        {
 
         }
 
+        @Override
+        public void surfaceCreated(SurfaceHolder pHolder)
+        {
+            m_Thread = new Thread(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    Canvas canvas = null;
+
+                    while (!Thread.currentThread().isInterrupted())
+                    {
+                        MoveRocks();
+
+                        canvas = m_SurfaceHolder.lockCanvas();
+                        canvas.drawColor(Color.parseColor("#75a2d9"));
+
+                        canvas.drawBitmap(m_RockBitmap, m_Rock1X, m_Rock1Y, m_Paint);
+                        canvas.drawBitmap(m_RockBitmap, m_Rock2X, m_Rock2Y, m_Paint);
+                        canvas.drawBitmap(m_RockBitmap, m_Rock3X, m_Rock3Y, m_Paint);
+                        canvas.drawBitmap(m_ShipBitmap, m_ShipX, m_ShipY, m_Paint);
+
+                        m_SurfaceHolder.unlockCanvasAndPost(canvas);
+                    }
+                }
+            });
+
+            m_Thread.start();
+        }
 
         @Override
         public void surfaceDestroyed(SurfaceHolder pHolder)
         {
-
+            if(m_Thread != null)
+            {
+                m_Thread.interrupt();
+            }
         }
     }
 }
